a4ab43c625b5ef9b8fafbb0d7a163f17da9ad75f
Peacefulhat <peacefulhat@gmail.com>
2025-01-20 17:46:55 +0530

    some additions

--- Haskell/first.hs
+++ Haskell/first.hs
@@ -1,11 +1,17 @@
 -- this is how we define functions.
-
+import Prelude (round)
 factorial :: Integer -> Integer
 factorial 0 = 1 --Base case
 factorial n = n * factorial (n-1)
 
 -- double function
 double x = 2*x
+-- round function.
+test::
+round 3.6
+
+round (-3.6)
+
 
 
 
