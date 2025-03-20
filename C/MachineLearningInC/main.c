c7c59e2f5966ec48bd9e2da2e496900a8b0c2b35
Peacefulhat <peacefulhat@gmail.com>
2025-03-20 07:13:14 +0530

    MachineLearningInC

---
+++ C/MachineLearningInC/main.c
@@ -0,0 +1,52 @@
+#include<time.h>
+#include"nn.h"
+#define NN_IMPLEMENTATION
+
+
+typedef struct{
+  Mat a0;
+  Mat w1,b1,a1;
+  Mat w2,b2,a2;
+}Xor;
+
+float forward_xor(Xor m, float x1,float x2){
+ MAT_AT(m.a0, 0, 0) = x1;
+ MAT_AT(m.a0, 0, 1) = x2;
+ 
+ // forwarding
+ mat_dot(m.a1,m.a0,m.w1);
+ mat_sum(m.a1,m.b1);
+ mat_sig(m.a1);
+
+ mat_dot(m.a2,m.a1,m.w2);
+ mat_sum(m.a2,m.b2);
+ mat_sig(m.a2);
+ 
+ return *m.a2.es;
+  
+}
+
+void xor_model(){
+  Xor m;
+  m.a0 = mat_alloc(1, 2);
+  m.w1 = mat_alloc(2,2);
+  m.b1 = mat_alloc(1,2);
+  m.w2 = mat_alloc(2, 1);
+  m.b2 = mat_alloc(1,1);
+  m.a1 = mat_alloc(1, 2);
+  m.a2 = mat_alloc(1, 1);
+ mat_rand(m.w1,0,1);
+ mat_rand(m.b1,0,1);
+ mat_rand(m.w2,0,1);
+ mat_rand(m.b2,0,1);
+ // MAT_PRINT(m.w1);
+ // MAT_PRINT(m.b1);
+ // MAT_PRINT(m.w2);
+ // MAT_PRINT(m.b2);
+}
+
+int main(void){
+ srand(time(0));
+ xor_model();
+  return 0;
+}
---
+++ C/MachineLearningInC/readme.txt
@@ -0,0 +1,15 @@
+
+I dont own anything ,this code belong to mr tsoding.
+This is me learning machine learning and c.
+
+
+I dont own anything ,this code belong to mr tsoding.
+This is me learning machine learning and c.
+
+I dont own anything ,this code belong to mr tsoding.
+This is me learning machine learning and c.
+
+I dont own anything ,this code belong to mr tsoding.
+This is me learning machine learning and c.
+
+
