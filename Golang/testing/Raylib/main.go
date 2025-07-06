ff15a7e4b109774d5a680a134c14cfac4cfd3b88
Peacefulhat <peacefulhat@gmail.com>
2025-07-06 05:44:40 +0530

    some additions

--- Golang/testing/Raylib/main.go
+++ Golang/testing/Raylib/main.go
@@ -5,11 +5,40 @@ import (
 	"fmt"
 )
 
+/*
+   #include<raylib.h>
+   #cgo LDFLAGS: -lraylib
+ */
+
 func main(){
-	if raylib.WindowCreate() == 0 {
-		fmt.Println("Everything Went Fine")
-	}else{
-		fmt.Println("Window Created Not Worked Fine")
-	}	
+		
+    const screenWidth = 800;
+    const screenHeight = 450;
+
+    C.InitWindow(screenWidth, screenHeight, C.CString("raylib [<module>] example - <name>"));
+    C.SetTargetFPS(60);
+	for; !bool(C.WindowShouldClose());{
+    
+        C.BeginDrawing();
+		C.ClearBackground(C.RAYWHITE);
+		C.DrawText(C.CString("Congrats! You created your first window!"), 190, 200, 20, C.LIGHTGRAY);
+
+        C.EndDrawing();
+
+	}
+	
+    C.CloseWindow(); 
+
+
+
+
+
+
+
+
+
+
+
+	
 
 }
--- Golang/testing/Raylib/raylib/raylib.go
+++ Golang/testing/Raylib/raylib/raylib.go
@@ -7,24 +7,5 @@ package raylib
 import "C"
 
 
-func WindowCreate() uint8{
-    const screenWidth = 800;
-    const screenHeight = 450;
-
-    C.InitWindow(screenWidth, screenHeight, C.CString("raylib [<module>] example - <name>"));
-
-    C.SetTargetFPS(60);
-	for; !bool(C.WindowShouldClose());{
-    
-        C.BeginDrawing();
-		C.ClearBackground(C.RAYWHITE);
-		C.DrawText(C.CString("Congrats! You created your first window!"), 190, 200, 20, C.LIGHTGRAY);
-
-        C.EndDrawing();
-
-	}
-	
-    C.CloseWindow(); 
-
-    return 0;
+func EventPolling() uint8{
 }
